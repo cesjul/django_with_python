702f1d0d108b6e1bec015f1aecf737632f7f7c57
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-03-27 14:08:11 -0300

    Inserindo postblocks de forma dinamica com for

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-
+from blog.data import posts
 
 
 def blog(request):
 
     context =  {'text': 'On initial blog page',
                 'title': 'Blog - ',
+                'posts': posts,
                 }
 
     return render(request,
