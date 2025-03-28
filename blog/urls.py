2ae569ff76a4edd779c62935614dfd1c330d4663
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-03-28 15:46:10 -0300

    Criando paginas para cada post

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from blog import views as blog_views
 app_name = 'blog'
 
 urlpatterns = [
-    path('post/<int:id>/', blog_views.post, name='post'),
+    path('post/<int:post_id>/', blog_views.post, name='post'),
     path('example/', blog_views.example, name='example'),
     path('', blog_views.blog, name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 from blog.data import posts
-
+from typing import Any
 
 def blog(request):
 
@@ -22,13 +23,22 @@ def example(request):
                   'blog/example.html', context,
     )
 
-def post(request, id):
+def post(request: HttpRequest, post_id: int): 
+    found_post:dict[str, Any] | None = None
+
+    for post in posts:
+        if post['id'] == post_id:
+            found_post = post
+            break
+
+    if found_post == None:
+        raise Exception('Post not founded')
 
     context =  {'text': 'On initial blog page',
-                'title': 'Blog - ',
-                'posts': posts,
+                'title': found_post['title'] + ' - ',
+                'post': found_post,
                 }
 
     return render(request,
-                  'blog/home.html',  context,
+                  'blog/post.html',  context,
     )
